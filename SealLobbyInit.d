f220dc6a2e506c81dbed522169ea2efae15f50e5
teguh <teguh.prabowo557@gmail.com>
2022-04-17 14:28:38 +0000

    init commit

---
+++ SealLobbyInit.d
@@ -0,0 +1,11 @@
+######################
+#  ¼ø¼­ ¹Ù²ÙÁã ¸»²¯  #
+######################
+#
+G_ServerName=LS0101
+#
+#G_ClientVersion=20080926
+#
+G_ClientVersion=20200818
+#G_ClientVersion=20090115
+#
